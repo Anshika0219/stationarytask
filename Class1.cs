c39dcb736129d3cc0d81fe4482706503c3afc52d
Anshika0219 <113534383+Anshika0219@users.noreply.github.com>
2022-09-30 10:31:51 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace program
+{
+    internal class store
+    {
+        private int id;
+        private string name;
+        private int no_of_branches;
+        private int no_of_employe;
+        private int no_of_manager;
+        private string location;
+        public int Id
+        {
+            get { return id;}
+            set {id = value;}
+        }
+        public string Name
+        {
+            get { return name;}
+            set { name = value;}
+
+            }
+        public int Noofbrances
+        {
+            get {return no_of_branches;}
+            set {no_of_branches = value; }
+        }
+        public int employe
+        {
+            get { return no_of_employe; }
+            set { no_of_employe = value; }
+        }
+        public int Noofmanager
+        {
+            get { return no_of_manager;}
+            set { no_of_manager = value;}
+        }
+        public string Location
+        {
+            get { return location;}
+            set {location = value;}
+        }
+        public void totalemploye()
+        {
+            int total = no_of_manager + no_of_employe + no_of_branches;
+            Console.WriteLine(total);
+        }
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    internal class program
+    {
+
+        static void Main(string[] args)
+        {
+            store s = new store();
+            s.Id = 1;
+            s.Name = "abc";
+            s.Noofbrances = 3;
+            s.Noofmanager = 4;
+            s.employe = 7;
+            s.Location = "HSR";
+            Console.WriteLine(s.Id);
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.Noofbrances);
+            Console.WriteLine(s.Noofmanager);
+            Console.WriteLine(s.employe);
+            Console.WriteLine(s.Location);
+            s.totalemploye();
+        }
+
+    }
+
+}
